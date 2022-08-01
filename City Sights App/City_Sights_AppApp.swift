aa99a9b605b65db8785da072b69f90d5398f74ad
Patel Krupa <patelkrupa209@gmail.com>
2022-08-02 09:44:55 +1000

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Krupa Patel on 2/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
